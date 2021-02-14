0a1efa6be0b7d99f3c02a5e2c064c36c21c1319d
Your Name <you@example.com>
2021-02-14 15:25:42 +0100

    added scroll to top and typing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ScrollToTop from "react-scroll-to-top";
+import { ReactComponent as UpArrow } from './images/upArrow.svg'
 
 function App() {
   return (
     <div className="">
+      <ScrollToTop smooth component={<UpArrow />} />
       <Navigation />
     </div>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,34 +7,43 @@ import Col from 'react-bootstrap/Col'
 import couple from '../../images/couple.jpg'
 import dots from '../../images/dots.svg'
 import PropertiesList from '../../components/home/PropertiesList'
+import Typed from 'react-typed';
 
 function Home() {
   return (
     <>
-  <div className="home">
-    <Container>
-      
-      <Row>
-        <Col sm={12} md={6}>
-          <div className="input-wrapper">
-            <h1>
-              Discover a place you'll love to live|
-            </h1>
-            <BigInput />
-          </div>
-        </Col>
-        <Col sm={12} md={6}>
-          <div className="img-wrapper">
-            <img className="img-hero " src={couple}/>
-          </div>
-          <img className="dots" style={{height:140}} src={dots}/>
-        </Col>
-      </Row>
-    </Container>
-  </div>
-    <Container fluid>
-      <PropertiesList />
-    </Container>
+      <div className="home">
+        <Container>
+
+          <Row>
+            <Col sm={12} md={6}>
+              <div className="input-wrapper">
+                <h1>
+                  Discover a place you'll
+                </h1>
+                <h1 style={{ color: "#007bff" }}>
+                  <Typed
+                    strings={['love to live', 'enjoy living']}
+                    typeSpeed={50}
+                    backSpeed={60}
+                    loop
+                  />
+                </h1>
+                <BigInput />
+              </div>
+            </Col>
+            <Col sm={12} md={6}>
+              <div className="img-wrapper">
+                <img className="img-hero " src={couple} />
+              </div>
+              <img className="dots" style={{ height: 140 }} src={dots} />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <Container fluid>
+        <PropertiesList />
+      </Container>
     </>
   );
 }
